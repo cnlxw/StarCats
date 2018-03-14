645acffaab304a15c0c48731543d6e97275ba273
Nikit Bobba <nikitbobba2019@u.northwestern.edu>
2018-03-14 11:36:29 -0500

    particle animation

--- StarCats/Assets/Scripts/PlanetA.cs
+++ StarCats/Assets/Scripts/PlanetA.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlanetA : MonoBehaviour {
+public class PlanetA : MonoBehaviour
+{
+
+	public GameObject whiteParticleGO;
 	
+
+	
+
 	private void OnTriggerEnter2D(Collider2D other)
 	{
 		
 		if (other.gameObject.name == "Player")
 		{
+			playParticle();
 
 			Destroy(gameObject);
 			ScoreManager.AddScore(10);
@@ -19,5 +26,11 @@ public class PlanetA : MonoBehaviour {
 			Destroy(gameObject);
 		}
 		
+	}
+	
+	void playParticle()
+	{
+		GameObject explosion = (GameObject) Instantiate(whiteParticleGO);
+		explosion.transform.position = transform.position;
 	}
 }
